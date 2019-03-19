951ac4352d6c43a36ce809dd6d9e0e0efbe50e9c
Angel Alberici <angel.alberici@angelalber-ltm2.internal.salesforce.com>
2019-03-19 09:03:16 +0000

    Added Content-Type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -7,13 +7,7 @@ import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
-	 
-	@Test
-	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-	   MuleEvent event = runFlow("retrieveFlights");
-	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
-	   assertEquals("application/json", contentType);
-	 }
+	
 	 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
